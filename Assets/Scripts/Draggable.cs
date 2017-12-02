a5e398d308cfb3d8be92d14de1245f8712729d1b
Unknown <asgerhb@gmail.com>
2017-12-02 18:54:26 +0100

    You can now drag stuff around

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Draggable : MonoBehaviour
+{
+	[HideInInspector]
+	public Rigidbody2D Rigidbody2D;
+	public Dragger DraggedBy;
+	public float Eagerness = 10;
+	public float CarryDrag = 2.5f;
+
+	private void Start()
+	{
+		Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (DraggedBy != null)
+		{
+			//transform.position = DraggedBy.TargetPosition;
+			if (Rigidbody2D != null)
+			{
+				Rigidbody2D.AddForce(
+					new Vector2(
+						DraggedBy.TargetPosition.x - transform.position.x,
+						DraggedBy.TargetPosition.y - transform.position.y)
+					* Eagerness * Time.deltaTime);
+				//Rigidbody2D.Sleep();
+				//Rigidbody2D.velocity = Vector3.zero;
+				Rigidbody2D.gravityScale = 0;
+				Rigidbody2D.drag = CarryDrag;
+			}
+		}
+		else
+		{
+			Rigidbody2D.gravityScale = 1;
+			Rigidbody2D.drag = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Dragger.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragger : MonoBehaviour
+{
+	private Camera _main;
+	private Draggable _draggable;
+	private Vector3 _mousePosition;
+	private Vector3 _previousMousePosition;
+	
+	public Vector3 TargetPosition;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_main = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_mousePosition = _main.ScreenToWorldPoint(Input.mousePosition);
+		
+		TargetPosition = new Vector3(_mousePosition.x, _mousePosition.y);
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Grab();
+		}
+
+		if (_draggable != null && Input.GetButtonUp("Fire1"))
+		{
+			Release();
+		}
+		
+		_previousMousePosition = _mousePosition;
+	}
+
+	void Grab()
+	{
+		RaycastHit2D ray = Physics2D.Raycast(_main.transform.position, _mousePosition);
+
+		if (ray.transform == null)
+			return;
+			
+		_draggable = ray.transform.GetComponent<Draggable>();
+
+		if (_draggable == null)
+			return;
+			
+		_draggable.DraggedBy = this;
+	}
+
+
+	void Release()
+	{
+		_draggable.DraggedBy = null;
+		
+		/*if (_draggable.Rigidbody2D != null)
+		{
+			_draggable.Rigidbody2D.velocity = 
+				new Vector2(
+					(_mousePosition.x - _previousMousePosition.x) * Time.deltaTime,
+					(_mousePosition.y - _previousMousePosition.y) * Time.deltaTime)
+				*100;
+		}*/
+		
+		_draggable = null;
+	}
+}

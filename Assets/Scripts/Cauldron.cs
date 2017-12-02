afb4b282bfd3ec82be90bd0bd7ac7585d18dd8ea
Unknown <asgerhb@gmail.com>
2017-12-02 19:26:16 +0100

    Cauldron that tracks what you put in it

---
+++ Assets/Scripts/Cauldron.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour
+{
+	public Dictionary<Reagent.ReagentType, int> Reagents = new Dictionary<Reagent.ReagentType, int>();
+
+	private void Start()
+	{
+		Reagents.Add(Reagent.ReagentType.Crystal, 0);
+		Reagents.Add(Reagent.ReagentType.Eyeball, 0);
+		Reagents.Add(Reagent.ReagentType.Mushroom, 0);
+		Reagents.Add(Reagent.ReagentType.Root, 0);
+		Reagents.Add(Reagent.ReagentType.Rose, 0);
+		Reagents.Add(Reagent.ReagentType.Skull, 0);
+		Reagents.Add(Reagent.ReagentType.Urn, 0);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Reagent reagent = other.GetComponent<Reagent>();
+		
+		if (reagent == null)
+			return;
+
+		Reagents[reagent.Type]++;
+		Destroy(reagent.gameObject);
+	}
+}
---
+++ Assets/Scripts/Reagent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reagent : MonoBehaviour {
+
+	public enum ReagentType
+	{
+		Urn,
+		Rose,
+		Crystal,
+		Mushroom,
+		Skull,
+		Root,
+		Eyeball,
+	}
+
+	public ReagentType Type;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

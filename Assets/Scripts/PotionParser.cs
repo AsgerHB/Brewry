5a667757f58fb5fc9ce7f596edde7fb5f3518482
Unknown <asgerhb@gmail.com>
2017-12-02 20:38:47 +0100

    Message when potion is done

---
+++ Assets/Scripts/PotionParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionParser : MonoBehaviour
+{
+
+	public Text TargetText;
+	public Cauldron Cauldron;
+	
+	public void ParsePotion()
+	{
+		Dictionary<Reagent.ReagentType, int> reagents = Cauldron.Reagents;
+		
+		TargetText.text = "The ingredients in your laboratory are either plastic, water, or both, " +
+		       "but the placebo effect keeps your customers happy";
+
+		TargetText.gameObject.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour
+{
+	private Camera _main;
+
+	public UnityEvent Pulled;
+	
+	void Start ()
+	{
+		_main = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Vector3 mousePosition = _main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D ray = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+			if (ray.transform != gameObject.transform)
+				return;
+			
+			Pulled.Invoke();
+		}
+	}
+}

b0af62e0cbfc324512ac7f77ffa675ffc3a09de8
Unknown <asgerhb@gmail.com>
2017-12-03 16:15:13 +0100

    Now removes stuff that moves outside screen

---
+++ Assets/Scripts/BoundsChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoundsChecker : MonoBehaviour
+{
+	public Vector2 XBounds;
+	public Vector2 YBounds;
+	public Vector2 ZBounds;
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (transform.position.x < XBounds.x || transform.position.x > XBounds.y ||
+		    transform.position.y < YBounds.x || transform.position.y > YBounds.y ||
+		    transform.position.z < ZBounds.x || transform.position.z > ZBounds.y)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+}

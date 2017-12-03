72c1725d8b648d60587cca764a15e0e06051ce26
Unknown <asgerhb@gmail.com>
2017-12-03 16:49:14 +0100

    Steeeeam!

---
+++ Assets/Scripts/Instantiater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiater : MonoBehaviour
+{
+	public GameObject ThingToInstantiate;
+	
+	public void DoTheThing()
+	{
+		GameObject thing = Instantiate(ThingToInstantiate);
+		thing.transform.position = transform.position;
+	}
+}
